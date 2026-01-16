b95f2e6c67b4d3ed77f180875a66993d1e24baa9
salonbooking123 <rishabhdubey192005@gmail.com>
2026-01-16 13:07:56 +0530

    Add files via upload

---
+++ ALL_CODE_LANG/JAVA/Account.java
@@ -0,0 +1,12 @@
+package mypackage;
+public class Account{
+    public String name;
+}
+class bank {
+    public static void main(System []args){
+        // System.out.println("hello");
+        Account ac=new Account();
+        ac.name="Sita";
+        System.out.println(ac.name);
+    }
+}
