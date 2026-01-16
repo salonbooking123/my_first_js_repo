8968de54ccea31d08d58191eb49f6c0ae535c65c
salonbooking123 <rishabhdubey192005@gmail.com>
2026-01-16 13:07:11 +0530

    Delete ALL_CODING_LANG. directory

--- ALL_CODING_LANG./ROUGH.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    //code here  h
-    cout<<7/3<<endl;// quotient
-    cout<<7%3<<endl; //remainder
-    // abcdefghijklmnopqrstuvwxyz.
-}
