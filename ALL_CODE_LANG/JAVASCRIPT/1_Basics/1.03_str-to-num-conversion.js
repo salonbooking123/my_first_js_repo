20caaf200635100696d2578292c8c3fb0eb8486f
salonbooking123 <rishabhdubey192005@gmail.com>
2026-01-16 13:13:26 +0530

    Add files via upload

---
+++ ALL_CODE_LANG/JAVASCRIPT/1_Basics/1.03_str-to-num-conversion.js
@@ -0,0 +1,21 @@
+// console.log(2+4);
+// console.log("2"+"4");
+// console.log(2*4)
+// console.log(2**2)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(2 >= 1); 
+// console.log(2 != 1);
+
+//wrong way of comparison or unusual answers while comparing different data types  i.e. avoid these kind of comparisons
+// console.log("2" == 1);
+// console.log("02" > 1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
+
+// console.log("2" == 2)    // it compares only values after type conversion
+// console.log("2" === 2)   // it compares values as well as data types without type conversion
